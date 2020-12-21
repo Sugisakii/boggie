aebf44e68495764bb4203891c1ce778f04d95d61
Pedro Hernandez <gamerpeter90@gmail.com>
2020-12-20 20:50:48 -0400

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,117 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <div class="container">
+        <div class="nk-gap-2"></div>
+        <div class="row vertical-gap">
+          <div class="col-lg-5">
+            <div class="nk-feature-1">
+              <div class="nk-feature-icon">
+                <img
+                  src="https://dejagerart.com/wp-content/uploads/2018/09/Test-Logo-Circle-black-transparent.png"
+                  width="90"
+                  height="90"
+                />
+              </div>
+              <div class="nk-feature-cont">
+                <h3 class="nk-feature-title">clan name</h3>
+                <a href="javascript:void(0)">
+                  <h4 class="nk-feature-title text-main-1">Stats del clan</h4>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-5">
+            <div class="nk-feature-1">
+              <div class="nk-feature-icon">
+                <img
+                  src="https://dejagerart.com/wp-content/uploads/2018/09/Test-Logo-Circle-black-transparent.png"
+                  width="90"
+                  height="90"
+                />
+              </div>
+              <div class="nk-feature-cont">
+                <h3 class="nk-feature-title">clan name</h3>
+                <a href="javascript:void(0)">
+                  <h4 class="nk-feature-title text-main-1">stats del clan</h4>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="nk-gap-2"></div>
+        <div class="row vertical-gap">
+          <div class="col-lg-8">
+            <div class="nk-gap-1"></div>
+            <h4 class="nk-decorated-h-2 nk-blog-post nk-blog-post-border-bottom">
+              <span
+                ><span class="text-main-1"><strong>Ultimas </strong></span
+                ><strong>Noticias</strong></span
+              >
+            </h4>
+            <div class="nk-blog-post nk-blog-post-border-bottom">
+              <div class="row vertical-gap">
+                <div class="col-md-5 col-lg-3">
+                  <div class="nk-post-img">
+                    <img
+                      src="https://dejagerart.com/wp-content/uploads/2018/09/Test-Logo-Circle-black-transparent.png"
+                    />
+                  </div>
+                </div>
+                <div class="col-md-7 col-lg-9">
+                  <h2 class="nk-post-title h4">
+                    <a href="javascript:void(0)">Titulo de la noticia</a>
+                  </h2>
+                  <div class="nk-post-date mt-10 mb-10">
+                    <span class="fa fa-calendar"></span
+                    ><label><strong>Jul 3, 2018</strong></label
+                    ><span class="fa fa-comments"></span
+                    ><a href="javascript:void(0)">0 comments</a>
+                    <div class="nk-post-text">
+                      <p>Descripcion de la noticia</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 offset-lg-0">
+            <aside class="nk-sidebar nk-sidebar-right nk-sidebar-sticky">
+              <div class="nk-widget">
+                <div class="nk-widget nk-widget-highlighted">
+                  <h4 class="nk-widget-title">
+                    <span
+                      ><span class="text-main-1"><strong>TOP GLOBAL </strong></span
+                      ><strong>5 Mejores</strong></span
+                    >
+                  </h4>
+                  <div class="nk-widget-content">
+                    <div class="nk-widget-post">
+                      <img
+                        class="nk-post-image"
+                        src="https://dejagerart.com/wp-content/uploads/2018/09/Test-Logo-Circle-black-transparent.png"
+                      />
+                      <h3 class="nk-post-title">
+                        <a href="javascript:void(0)">nombre del jugador</a>
+                      </h3>
+                      <div class="nk-product-price"><label>Ranking #1</label></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
