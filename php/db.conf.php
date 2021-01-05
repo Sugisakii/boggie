97719e809e0f48381e9106b5c49ad6730b3971c6
Pedro Hernandez <gamerpeter90@gmail.com>
2021-01-05 10:11:25 -0400

    Update db.conf.php

--- php/db.conf.php
+++ php/db.conf.php
@@ -1,12 +1,11 @@
 <?php
 require_once('./class.php');
 $HOST = "localhost";
-$USER = "root";
-$PASS = "!8%uHoZ7w!";
-$DB = "nuevaweb";
+$USER = "web";
+$PASS = "y2V2r3KNP#u2";
+$DB = "web";
 $api = new api();
 $mysql = new mysqli($HOST, $USER, $PASS, $DB);
 if($mysql->connect_errno) {
     $api->DropError($mysql->connect_error);
 }
-?>
