5444925db2793a0b0622bbb840f9df6db08bff73
Pedro Hernandez <gamerpeter90@gmail.com>
2021-01-12 09:37:09 -0400

    Home Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -40,66 +40,147 @@
           </div>
         </div>
         <div class="nk-gap-2"></div>
-        <div class="row vertical-gap">
-          <div class="col-lg-8">
-            <div class="nk-gap-1"></div>
-            <h4 class="nk-decorated-h-2 nk-blog-post nk-blog-post-border-bottom">
-              <span
-                ><span class="text-main-1"><strong>Ultimas </strong></span
-                ><strong>Noticias</strong></span
-              >
-            </h4>
-            <div class="nk-blog-post nk-blog-post-border-bottom">
-              <div class="row vertical-gap">
-                <div class="col-md-5 col-lg-3">
-                  <div class="nk-post-img">
-                    <img
-                      src="https://dejagerart.com/wp-content/uploads/2018/09/Test-Logo-Circle-black-transparent.png"
-                    />
-                  </div>
-                </div>
-                <div class="col-md-7 col-lg-9">
-                  <h2 class="nk-post-title h4">
-                    <a href="javascript:void(0)">Titulo de la noticia</a>
-                  </h2>
-                  <div class="nk-post-date mt-10 mb-10">
-                    <span class="fa fa-calendar"></span
-                    ><label><strong>Jul 3, 2018</strong></label
-                    ><span class="fa fa-comments"></span
-                    ><a href="javascript:void(0)">0 comments</a>
-                    <div class="nk-post-text">
-                      <p>Descripcion de la noticia</p>
+        <div class="nk-main">
+          <div class="nk-gap-2"></div>
+          <div class="container">
+            <div class="row vertical-gap">
+              <div class="col-lg-8">
+                <h3 class="nk-decorated-h-2">
+                  <span><span class="text-main-1">ultimas</span> noticias</span>
+                </h3>
+                <div class="nk-gap"></div>
+                <div class="nk-tabs">
+                  <div class="tab-content">
+                    <div class="nk-blog-post nk-blog-post-border-bottom">
+                      <a class="nk-post-img" href="javascript:void(0)"
+                        ><img
+                          src="https://www.workana.com/i/wp-content/uploads/2019/02/ab-testing-split-min.jpg"
+                        /><span
+                          class="nk-post-categories"
+                          style="
+                            border: 2px none rgb(0, 0, 0);
+                            border-left-style: none;
+                          "
+                          ><span class="text-uppercase bg-main-1"
+                            >Categoria</span
+                          ></span
+                        ></a
+                      >
+                      <div class="nk-gap-1"></div>
+                      <h2 class="nk-post-title h4">
+                        <a href="javscript:void(0)"
+                          ><strong>Titulo Ultima Noticia</strong></a
+                        >
+                      </h2>
+                      <div class="nk-post-date mt-10 mb-10">
+                        <span class="fa fa-calendar">Sep 5, 2018</span
+                        ><span class="fa fa-comments">0 Comentarios</span>
+                      </div>
+                      <div class="nk-post-text">
+                        <p>Descripcion de la ultima noticia...</p>
+                      </div>
+                    </div>
+                    <div class="nk-blog-post nk-blog-post-border-bottom">
+                      <div class="row vertical-gap">
+                        <div class="col-lg-3 col-md-5">
+                          <a class="nk-post-img" href="javascrip:void(0)"
+                            ><img
+                              src="https://economipedia.com/wp-content/uploads/test-de-estr%C3%A9s.png"
+                            /><span class="nk-post-categories"
+                              ><span class="text-uppercase bg-main-1"
+                                >categoria</span
+                              ></span
+                            ></a
+                          >
+                        </div>
+                        <div class="col-lg-9 col-md-7">
+                          <h2 class="nk-post-title h4">
+                            <a href="javascript:void(0)"
+                              ><strong>Titulo Ultima Noticia</strong></a
+                            >
+                          </h2>
+                          <div class="nk-post-date mt-10 mb-10">
+                            <span class="fa fa-calendar">Sep 5, 2018</span
+                            ><span class="fa fa-comments">0 Comentarios</span>
+                          </div>
+                          <div class="nk-post-text">
+                            <p>Descripcio de la noticia....</p>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
+              </div>
+              <div class="col-lg-4">
+                <aside class="nk-sidebar nk-sidebar-right nk-sidebar-sticky">
+                  <div class="nk-widget nk-widget-highlighted">
+                    <h4 class="nk-widget-title">
+                      <span
+                        ><span class="text-main-1"
+                          ><strong>REDES </strong></span
+                        >
+                        sociales</span
+                      >
+                    </h4>
+                    <div class="nk-widget-content">
+                      <ul class="nk-social-links-3 nk-social-links-cols-4">
+                        <li>
+                          <a class="nk-social-twitch" href="javascript:void(0)"
+                            ><span class="fab fa-twitch"></span
+                          ></a>
+                        </li>
+                        <li>
+                          <a
+                            class="nk-social-instagram"
+                            href="javascript:void(0)"
+                            ><span class="fab fa-instagram"></span
+                          ></a>
+                        </li>
+                        <li>
+                          <a
+                            class="nk-social-facebook"
+                            href="javascript:void(0)"
+                            ><span class="fab fa-facebook"></span
+                          ></a>
+                        </li>
+                        <li>
+                          <a class="nk-social-steam" href="javascript:void(0)"
+                            ><span class="fab fa-steam"></span
+                          ></a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="nk-widget nk-widget-highlighted">
+                    <h4 class="nk-widget-title">
+                      <span
+                        ><span class="text-main-1"
+                          ><strong>TOP GLOBAL</strong></span
+                        >
+                        5 mejores</span
+                      >
+                    </h4>
+                    <div class="nk-widget-content">
+                      <div class="nk-widget-post">
+                        <a class="nk-post-image" href="javascript:void(0)"
+                          ><img
+                            src="https://economipedia.com/wp-content/uploads/test-de-estr%C3%A9s.png"
+                        /></a>
+                        <h3 class="nk-post-title">
+                          <a href="javascript:void(0)"
+                            >el señor de los frescos</a
+                          >
+                        </h3>
+                        <div class="nk-product-price">
+                          <label>RANK #1</label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </aside>
               </div>
             </div>
-          </div>
-          <div class="col-lg-4 offset-lg-0">
-            <aside class="nk-sidebar nk-sidebar-right nk-sidebar-sticky">
-              <div class="nk-widget">
-                <div class="nk-widget nk-widget-highlighted">
-                  <h4 class="nk-widget-title">
-                    <span
-                      ><span class="text-main-1"><strong>TOP GLOBAL </strong></span
-                      ><strong>5 Mejores</strong></span
-                    >
-                  </h4>
-                  <div class="nk-widget-content">
-                    <div class="nk-widget-post">
-                      <img
-                        class="nk-post-image"
-                        src="https://dejagerart.com/wp-content/uploads/2018/09/Test-Logo-Circle-black-transparent.png"
-                      />
-                      <h3 class="nk-post-title">
-                        <a href="javascript:void(0)">nombre del jugador</a>
-                      </h3>
-                      <div class="nk-product-price"><label>Ranking #1</label></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </aside>
           </div>
         </div>
       </div>
