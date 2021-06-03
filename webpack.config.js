f665e085ae3823ce15b76f9b8c1bcde9fe28129d
Pedro Hernandez <gamerpeter90@gmail.com>
2021-06-02 21:00:30 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-    //...
-    optimization: {
-      splitChunks: {
-        // include all types of chunks
-        chunks: 'all'
-      }
+  //...
+  optimization: {
+    splitChunks: {
+      // include all types of chunks
+      chunks: 'all',
+      maxSize: 20000,
+      minSize: 10000
     }
-  };
+  }
+};
